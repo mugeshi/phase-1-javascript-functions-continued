5989f2e7100daf31c3c6a396d3cc6fb13823409e
Njeri Mugeshi <nicolemugeshi@gmail.com>
2023-06-15 05:51:46 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // code your solution here
+// Implementing the saturdayFun function
+function saturdayFun(activity = "roller-skate") {
+  return `This Saturday, I want to ${activity}!`;
+}
+
+// Implementing the mondayWork function
+const mondayWork = function(activity = "go to the office") {
+  return `This Monday, I will ${activity}.`;
+}
